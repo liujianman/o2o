f4f188047bfc3e63af89ccc1d8369c4115adb70d
liujianman <594951538@qq.com>
2018-01-02 20:44:55 +0800

    init demo

---
+++ src/main/java/com/kgy/o2o/entity/Area.java
@@ -0,0 +1,59 @@
+package com.kgy.o2o.entity;
+
+import java.util.Date;
+
+/**
+ * Created by ljm on 2018/1/2.
+ */
+public class Area {
+    //区域ID
+    private Integer areaId;
+    //区域名称
+    private String areaName;
+    //权重，越大排名越靠前
+    private Integer priority;
+    //创建时间
+    private Date createTime;
+    //更新时间
+    private Date lastEditTime;
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+}
