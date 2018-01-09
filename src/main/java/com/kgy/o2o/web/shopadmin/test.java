148e00df33b08d2a1d621f0428044331b6ec4905
liujianman <594951538@qq.com>
2018-01-09 11:03:22 +0800

    update test

---
+++ src/main/java/com/kgy/o2o/web/shopadmin/test.java
@@ -0,0 +1,13 @@
+package com.kgy.o2o.web.shopadmin;
+
+/**
+ * @Author: 刘健明
+ * @Description:
+ * @Date: Created in 10:41 2018/1/9
+ */
+public class test {
+    //测试
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
